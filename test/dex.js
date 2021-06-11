1bb81dba5921847d700135281546a9452410a409
nonusae <nonusae@gmail.com>
2021-06-11 20:03:17 +0700

    Allocate token to each users

--- test/dex.js
+++ test/dex.js
@@ -4,8 +4,9 @@ const Rep = artifacts.require('mock/Rep.sol');
 const Zrx = artifacts.require('mock/Zrx.sol');
 const Dex = artifacts.require('Dex.sol');
 
-contract('Dex', () => {
+contract('Dex', (accounts) => {
   let dai, bat, rep, zrx;
+  const [trader1, trader2] = [accounts[1], accounts[2]];
   const [DAI, BAT, REP, ZRX] = ['DAI', 'BAT', 'REP', 'ZRX'].map((ticker) =>
     web3.utils.fromAscii(ticker),
   );
@@ -25,5 +26,19 @@ contract('Dex', () => {
       dex.addToken(REP, rep.address),
       dex.addToken(ZRX, zrx.address),
     ]);
+
+    const amount = web3.utils.toWei('1000');
+    const seedTokenBalance = async (token, trader) => {
+      await token.faucet(trader, amount);
+      await token.approve(dex.address, amount, trader);
+    };
+
+    await Promise.all(
+      [dai, bat, rep, zrx].map((token) => seedtokenBalance(token, trader1)),
+    );
+
+    await Promise.all(
+      [dai, bat, rep, zrx].map((token) => seedtokenBalance(token, trader2)),
+    );
   });
 });
