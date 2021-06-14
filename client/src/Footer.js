b1c71fd9f9d4ab462c57ccb4bc31c2deac300c78
nonusae <nonusae@gmail.com>
2021-06-14 17:47:13 +0700

    Add client side folder for react frontend

---
+++ client/src/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p className="text-center">
+        From{' '}
+        <a href="http://pro.eattheblocks.com" style={{ color: 'white' }}>
+          Solidity is cool
+        </a>
+        Dex application.
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
